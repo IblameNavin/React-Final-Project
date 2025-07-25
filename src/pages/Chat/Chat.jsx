ecb7e75d0ec40a9f18ed4f8b9ecdf674058ce80f
iblameNavin <ayernavin11@gmail.com>
2025-07-25 22:54:38 +0545

    Develop Chat page layout and UI

---
+++ src/pages/Chat/Chat.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ChatInterface from '../../components/ChatInterface/ChatInterface';
+import './Chat.css';
+
+const Chat = () => {
+  return (
+    <div className="chat-page">
+      <ChatInterface />
+    </div>
+  );
+};
+
+export default Chat;
